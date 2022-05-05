42e2c27ba0f90cb18b3da46fe4dad91427785852
Upi Tamminen <desaster@gmail.com>
2022-05-05 10:13:02 +0300

    allow specifying nodejs path for neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,6 +10,17 @@
 
 local cmd = vim.cmd
 
+-- use node from nvm if version number is specified
+-- export NEOVIM_NODE_VERSION=v16.14.2
+-- to verify:
+--   :checkhealth
+if (vim.fn.has('unix') == 1 and vim.fn.empty(vim.env.NEOVIM_NODE_VERSION) == 0) then
+    local node_dir = vim.env.HOME .. '/.nvm/versions/node/' .. vim.env.NEOVIM_NODE_VERSION .. '/bin/'
+    if (vim.fn.isdirectory(node_dir)) then
+        vim.env.PATH = node_dir .. ':' .. vim.env.PATH
+    end
+end
+
 require("packer_setup")
 require("mappings")
 require("options")
