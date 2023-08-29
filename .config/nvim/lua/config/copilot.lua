bab1c13c34f4bfdd7e5869135fea972ea4fb8a8d
Upi Tamminen <desaster@gmail.com>
2023-08-29 13:17:28 +0300

    disable copilot panel open keybinding for now

--- .config/nvim/lua/config/copilot.lua
+++ .config/nvim/lua/config/copilot.lua
@@ -3,7 +3,13 @@ if not status_ok then
     return
 end
 
+-- HINT: :Copilot suggestion to toggle auto trigger
 copilot.setup({
+    panel = {
+        keymap = {
+            open = false, -- annoying to have it M-CR, TODO: find some other keybind for it
+        }
+    },
     suggestion = {
         auto_trigger = false,
         keymap = {
