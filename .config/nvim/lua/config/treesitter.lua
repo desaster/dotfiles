bf55ec3e2bdae0e6161e97fd6d983f7914626b77
Upi Tamminen <desaster@gmail.com>
2024-01-23 09:20:13 +0200

    add tsx to treesitter

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -5,7 +5,7 @@ end
 
 configs.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "c", "lua", "javascript", "typescript" },
+    ensure_installed = { "c", "lua", "javascript", "typescript", "tsx" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
