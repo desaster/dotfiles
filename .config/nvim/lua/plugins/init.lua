691c9c0cd45676965a78180e845371569967f63c
Upi Tamminen <desaster@gmail.com>
2024-04-17 09:32:23 +0300

    neovim: add copilot chat

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -124,6 +124,21 @@ return {
         config = function() require('config/copilot') end,
     },
 
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+        },
+        opts = {
+            debug = true, -- Enable debugging
+            -- See Configuration section for rest
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+        -- TODO: add config file
+    },
+
     -- colorize hex codes
     {
         'NvChad/nvim-colorizer.lua',
