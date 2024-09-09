5e4450e0507ce33d11d15904008de9e1bc4de5b0
Urho Tamminen <urho.tamminen@gofore.com>
2024-09-09 09:35:45 +0300

    add mise support for setting node version for neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,9 +14,20 @@
 -- to verify:
 --   :checkhealth
 if vim.fn.has('unix') == 1 and vim.fn.empty(vim.env.NEOVIM_NODE_VERSION) == 0 then
-    local node_dir = vim.env.HOME .. '/.nvm/versions/node/' .. vim.env.NEOVIM_NODE_VERSION .. '/bin/'
-    if (vim.fn.isdirectory(node_dir)) then
-        vim.env.PATH = node_dir .. ':' .. vim.env.PATH
+    local preferred_version = vim.env.NEOVIM_NODE_VERSION
+    if string.sub(preferred_version, 1, 1) == 'v' then
+        preferred_version = string.sub(preferred_version, 2)
+    end
+
+    -- primarily use nvm
+    local nvm_node_dir = vim.env.HOME .. '/.nvm/versions/node/v' .. preferred_version .. '/bin/'
+    -- alternatively mise
+    local mise_node_dir = vim.env.HOME .. '/.local/share/mise/installs/node/' .. preferred_version .. '/bin/'
+
+    if (vim.fn.isdirectory(nvm_node_dir) ~= 0) then
+        vim.env.PATH = nvm_node_dir .. ':' .. vim.env.PATH
+    elseif (vim.fn.isdirectory(mise_node_dir) ~= 0) then
+        vim.env.PATH = mise_node_dir .. ':' .. vim.env.PATH
     end
 end
 
