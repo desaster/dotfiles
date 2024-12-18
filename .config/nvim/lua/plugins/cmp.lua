33073f191a866e60873ee74c8860557dbefd09eb
Upi Tamminen <desaster@gmail.com>
2024-12-18 12:32:27 +0200

    try using merge from blink-cmp configuration
    Startup was throwing this error:
    
      Failed to run `config` for blink.cmp
      ...share/nvim/lazy/blink.cmp/lua/blink/cmp/config/utils.lua:10: completion.menu.draw.treesitter: expected table, got boolean
    
    So maybe merging the config will preserve the default treesitter
    options?

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -2,24 +2,27 @@ return {
     {
         -- https://github.com/saghen/blink.cmp
         "saghen/blink.cmp",
-        opts = {
-            -- https://github.com/saghen/blink.cmp?tab=readme-ov-file#configuration
-            completion = {
-                accept = {
-                    auto_brackets = {
-                        enabled = false,
+        opts = function(_, opts)
+            local ret = {
+                -- https://github.com/saghen/blink.cmp?tab=readme-ov-file#configuration
+                completion = {
+                    accept = {
+                        auto_brackets = {
+                            enabled = false,
+                        },
+                    },
+                    list = {
+                        selection = 'manual' -- do not preselect completion items
                     },
                 },
-                list = {
-                    selection = 'manual' -- do not preselect completion items
+                keymap = {
+                    preset = 'enter',
+                    -- Cycle selections with tab, shift-tab
+                    ['<Tab>'] = { 'select_next', 'fallback' },
+                    ['<S-Tab>'] = { 'select_prev', 'fallback' },
                 },
-            },
-            keymap = {
-                preset = 'enter',
-                -- Cycle selections with tab, shift-tab
-                ['<Tab>'] = { 'select_next', 'fallback' },
-                ['<S-Tab>'] = { 'select_prev', 'fallback' },
-            },
-        },
-    }
+            }
+            return LazyVim.merge({}, opts, ret)
+        end,
+    },
 }
