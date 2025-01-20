15adf5bef487fc6017a381397ca56b24feeb298c
Upi Tamminen <desaster@gmail.com>
2025-01-20 09:18:01 +0200

    disable conceallevel for markdown

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- disable conceal for markdown to prevent hiding link information
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "markdown" },
+    callback = function()
+        vim.wo.conceallevel = 0
+    end,
+})
