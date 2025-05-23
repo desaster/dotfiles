5637c5ea7a0f5f815ea96cf2e7f0bf7068ce50bf
Upi Tamminen <desaster@gmail.com>
2025-05-23 07:06:06 +0300

    neovim: update packages

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,6 @@
 return {
     {
         "williamboman/mason.nvim",
-        version = "^1.0.0", -- pinned version as a workaround https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
         opts = {
             ensure_installed = {
                 "stylua",
@@ -13,9 +12,5 @@ return {
                 "vtsls",
             },
         },
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        version = "^1.0.0", -- pinned version as a workaround
     },
 }
