f3801a18d68d72718f1013049a830190f15ea431
Upi Tamminen <desaster@gmail.com>
2025-11-20 13:03:23 +0200

    update neovim packages

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,21 +10,20 @@ return {
                 end
             end
 
-            local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-            keys[#keys + 1] = { "<F8>", diagnostic_goto(true) }
-            keys[#keys + 1] = { "<S-F8>", diagnostic_goto(false) }
-            -- Actually, shift-F8 may appear as F20
-            -- 1. check the escape code with: showkey -a
-            -- 2. create a debug log with: nvim -V3log
-            -- 3 .search for the escape code under --- Terminal info ---
-            keys[#keys + 1] = { "<F20>", diagnostic_goto(false) }
-            -- keys[#keys + 1] = { "<...>", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-            -- keys[#keys + 1] = { "<...>", map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-            -- keys[#keys + 1] = { "<...>", map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-            -- keys[#keys + 1] = { "<...>", map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-
             local ret = {
+                servers = {
+                    ['*'] = {
+                        keys = {
+                            { "<F8>", diagnostic_goto(true) },
+                            { "<S-F8>", diagnostic_goto(false) },
+                            -- Actually, shift-F8 may appear as F20
+                            -- 1. check the escape code with: showkey -a
+                            -- 2. create a debug log with: nvim -V3log
+                            -- 3 .search for the escape code under --- Terminal info ---
+                            { "<F20>", diagnostic_goto(false) }
+                        }
+                    }
+                },
                 diagnostics = {
                     -- disable virtual text, it often doesn't fit anyway
                     virtual_text = false,
